2659f9888b0533b4e2638ae11b2aaaff1dd69fd0
PratikDive <107669527+PratikDive@users.noreply.github.com>
2022-08-04 13:38:55 +0530

    Add files via upload

---
+++ Lab session/TotalNumberOfChar.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class TotalNumberOfChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = "Pratik";
+		int s2 =s1.length();
+		System.out.println(s2);
+		
+	}
+
+}
